34101e4d2c9021541b09db1930d076b0f3329620
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-01-25 21:52:16 +0900

    2-2

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer(
-  (req, res) => {
-    res.end('Hello World.')
-  }
-)
+const getFromClient = (req, res) => {
+  fs.readFile('./index.html', 'UTF-8', (error, data) => {
+    const content = data.replace(/replaced/g, '置換されたテキスト')
+    res.writeHead(200, { 'Content-Type': 'text/html'})
+    res.write(content)
+    res.end()
+  })
+}
+
+const server = http.createServer(getFromClient)
 
 server.listen('3000')
 
